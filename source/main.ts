771b48192280c51ce863cd7dcb8caead9f9ee64d
Oliver Salzburg <oliver.salzburg@gmail.com>
2024-05-18 16:18:55 +0200

    feat: Initital code import

--- source/main.ts
+++ source/main.ts
@@ -1,1 +1,38 @@
-export const nothing = undefined;
+import core from "@actions/core";
+import github from "@actions/github";
+import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/error/console.js";
+import { CommitValidator } from "./CommitValidator.js";
+
+const isMainModule = import.meta.url.endsWith(process.argv[1]);
+
+/**
+ * Execute the commit validator action.
+ */
+export const main = async (): Promise<void> => {
+  try {
+    if (!process.env.GITHUB_REPOSITORY) {
+      throw new Error("Missing GITHUB_REPOSITORY");
+    }
+
+    const prNumber = github.context.payload.number as string | undefined;
+    if (!prNumber) {
+      throw new Error("Unable to determine PR number!");
+    }
+
+    const repo_token = core.getInput("repo_token", { required: true });
+    const commitValidator = new CommitValidator({
+      core,
+      context: github.context,
+      octokit: github.getOctokit(repo_token),
+      pr: Number.parseInt(prNumber),
+    });
+
+    await commitValidator.main();
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+};
+
+if (isMainModule) {
+  main().catch(redirectErrorsToConsole(console));
+}
