d2cfd670ac54e14f527564c56afdf017f567c7bd
Oliver Salzburg <oliver.salzburg@gmail.com>
2026-02-03 10:45:22 +0100

    fix: imports

--- source/main.ts
+++ source/main.ts
@@ -1,4 +1,4 @@
-import core from "@actions/core";
+import * as core from "@actions/core";
 import github from "@actions/github";
 import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/errors/console.js";
 import { CommitValidator } from "./CommitValidator.js";
